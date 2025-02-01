a5e1bb5dd25b49a0ef01109d4226f942fad6a65c
HectorRosero <hlroserog@gmail.com>
2025-01-31 20:53:13 -0500

    first commit

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,10 +3,14 @@ const url = 'http://localhost:3000/api/admin/users';
 
 document.addEventListener('DOMContentLoaded', () => {
     const token = localStorage.getItem('token'); // Obtener el token almacenado en el localStorage
-    const decodedToken = JSON.parse(atob(token.split('.')[1])); // Decodificamos el token para obtener los datos
+    if (!token) {
+        window.location.href = 'login.html'; // Me redirige sino hay token
+        return;
+    }
+    const decodedToken = JSON.parse(atob(token.split('.')[1])); // Decodifica el token para obtener los datos
     const userRole = decodedToken.role;
-    const usuario = decodedToken.usuario; // Extraemos el nombre de usuario
-    const id = decodedToken.id; // Extraemos el id del usuario
+    const usuario = decodedToken.usuario; // Extrae el nombre de usuario
+    const id = decodedToken.id; // Extrae el id del usuario
 
     const accountIcon = document.getElementById('account-icon');
     const userInfoDiv = document.getElementById('user-info');
@@ -16,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
   
     accountIcon.addEventListener('click', () => {
-      // Al hacer clic en el icono, mostramos los detalles del usuario
+      // Al hacer clic en el icono, mostra los detalles del usuario
       userInfoDiv.innerHTML = `
         <p><strong>Usuario:</strong> ${usuario}</p>
         <p><strong>Rol:</strong> ${userRole}</p>
@@ -28,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
    
 
 async function loadClinics() {
-    const response = await fetch('/api/clinics'); // Asegúrate de tener esta ruta en tu API
+    const response = await fetch('/api/clinics'); // Asegurar de tener esta ruta en mi API
     const clinics = await response.json();
     const clinicSelect = document.getElementById('clinicSelect');
 
@@ -65,10 +69,11 @@ document.getElementById('createRecordForm').addEventListener('submit', async (e)
     }
 });
 
-document.getElementById('logoutBtn').addEventListener('click', async(e) => {
+//Lógica de cierre de sesión
+document.getElementById('logoutBtn').addEventListener('click', (e) => {
     e.preventDefault();
     localStorage.removeItem('token');
     localStorage.removeItem('user');
-    window.location.href = '../index.html';
+    window.location.href = '../index.html'; //Asegurarme de que el nombre del archivo este bien escrito
   });
 });
--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,4 @@
 const url = 'http://localhost:3000/api/auth/login'; 
-
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
     e.preventDefault();
     const username = document.getElementById('username').value;
@@ -14,7 +13,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     const data = await response.json();
     if (response.ok) {
         localStorage.setItem('token', data.token); // Almacena el token
-            localStorage.setItem('user', JSON.stringify(data.username)); // Guardar los datos del usuario (nombre, email, rol)
+        localStorage.setItem('user', JSON.stringify(data.username)); // Guardar los datos del usuario (nombre, email, rol)
             
             // Decodificar el token JWT para obtener el rol del usuario
             const decodedToken = JSON.parse(atob(data.token.split('.')[1])); // Decodifica el token JWT
